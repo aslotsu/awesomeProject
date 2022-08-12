51dea1632bf41d2e83030073e9ce9cf6fa2ab142
Alfred Lotsu <alfrdlotsu2@gmail.com>
2022-08-12 00:49:42 +0000

    big and small set up

---
+++ big/big.go
@@ -0,0 +1,10 @@
+package big
+
+import "fmt"
+
+func Declare() {
+	fmt.Println("I have a big package baby!")
+}
+func Double(a int) int {
+	return a * 2
+}
---
+++ small/small.go
@@ -0,0 +1,11 @@
+package small
+
+import "fmt"
+
+func Declare() {
+	fmt.Println("I have a small package baby!")
+}
+
+func Half(a float64) float64 {
+	return a / 2
+}
